244f8f4c53ea91c409496cc6e638a2cddda7d656
Mor Marzan <mormarzan@gmail.com>
2024-06-07 02:08:04 +0300

    Add respnsive style for edit cmp

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -11,12 +11,12 @@ export function HomePage() {
             <section className="main-layout full hero">
                 <div className="section-container grid">
                     {user ?
-                        <div className="grid">
+                        <div className="grid user-section">
                             <h4 className="title">Hey {user.fullname}, Welcome back to Expensify!</h4>
                             <Link to="/expense" className="btn start">Check your Expenses</Link>
                         </div>
                         :
-                        <div className="grid">
+                        <div className="grid user-section">
                             <h4 className="title">Welcome to Expensify!</h4>
                             <h6 className="content">Start tracking your expenses today to lead a more organized and prosperous lifestyle</h6>
                             <LoginSignup />
